5d409994ab47c3405b9aca14cae3eea458a783a9
Pujan Shakya <puzann9843318452@gmail.com>
2024-03-21 01:33:16 -0700

    SCRUM-270 AddRoom

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -24,6 +24,24 @@ async function getRoom(orderId) {
   }
 }
 
+async function addRoom(room) {
+  try {
+    console.log(room);
+    let pool = await sql.connect(config);
+    let insertProduct = await pool
+      .request()
+      .input("RoomName", sql.NVarChar, room.RoomName)
+      .input("RoomTypeID", sql.Int, room.RoomTypeID)
+      .input("Floor", sql.Int, room.Floor)
+      .input("RoomStatus", sql.NVarChar, room.RoomStatus)
+      .input("RoomImageUrl", sql.NVarChar, room.RoomImageUrl)
+      .execute("CreateRoom");
+    return insertProduct.recordsets;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 async function updateRoom(room) {
   try {
     console.log(room);
