60b7f3e94219b00c12658eece1bb87464bc3621e
Pujan Shakya <puzann9843318452@gmail.com>
2024-03-21 01:28:09 -0700

    SCRUM-270 feat: Add UpdateRoom

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,26 +1,32 @@
-
-const dboperations = require('../controllers/roomController');
-var express = require('express');
+const dboperations = require("../controllers/roomController");
+var express = require("express");
 var router = express.Router();
 
-router.route('/all').get((request,response)=>{
-console.log("getting all rooms");
-    dboperations.getRooms().then(result => {
-       response.json(result[0]);
-    })
-})
+router.route("/all").get((request, response) => {
+  console.log("getting all rooms");
+  dboperations.getRooms().then((result) => {
+    response.json(result[0]);
+  });
+});
 
-router.route('/getroom/:id').get((request,response)=>{
-    dboperations.getRoom(request.params.id).then(result => {
-       response.json(result[0]);
-    })
-})
+router.route("/getroom/:id").get((request, response) => {
+  dboperations.getRoom(request.params.id).then((result) => {
+    response.json(result[0]);
+  });
+});
 
-router.route('/addroom').post((request,response)=>{
-    let order = {...request.body}
-    dboperations.addRoom(order).then(result => {
-       response.status(201).json(result);
-    })
-})
+router.route("/addroom").post((request, response) => {
+  let order = { ...request.body };
+  dboperations.addRoom(order).then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+router.route("/updateroom").put((request, response) => {
+  let room = { ...request.body };
+  dboperations.updateRoom(order).then((result) => {
+    response.status(201).json(result);
+  });
+});
 
 module.exports = router;
