8d4955ecd10bbd268a3a6d578c41e1b55b690e30
Pujan Shakya <puzann9843318452@gmail.com>
2024-03-28 17:33:07 -0700

    SCRUM-317 Add Employee Id

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,49 +1,75 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const bcrypt = require("bcrypt");
 const sql = require("mssql");
 var config = require("../dbconfig");
-const login = async (req, res) =>  {
+const login = async (req, res) => {
   try {
-     const { employeeid, password } = req.body;
- 
-  //  const hashedPassword = await bcrypt.hash( password, 10);
-      const pool = await sql.connect(config);
-      const result = await pool.request()
-      .input('Username', sql.VarChar, employeeid)
-     .execute('Getuser');
+    const { employeeid, password } = req.body;
 
-   console.log(result.recordset[0].Password);
+    //  const hashedPassword = await bcrypt.hash( password, 10);
+    const pool = await sql.connect(config);
+    const result = await pool
+      .request()
+      .input("Username", sql.VarChar, employeeid)
+      .execute("Getuser");
 
-   const match = await bcrypt.compare(password, result.recordset[0].Password);
+    console.log(result.recordset[0].Password);
+
+    const match = await bcrypt.compare(password, result.recordset[0].Password);
 
     //   const status = result.recordsets[0][0].Status;
     //   const userID = result.recordsets[0][0].UserID;
 
-    const userID =result.recordset[0].ID;
-    const FirstName =result.recordset[0].FirstName;
-    const LastName =result.recordset[0].LastName;
-    const EmployeeType =result.recordset[0].EmployeeType;
+    console.log(result.recordset[0]);
+    const userID = result.recordset[0].ID;
+    const FirstName = result.recordset[0].FirstName;
+    const LastName = result.recordset[0].LastName;
+    const EmployeeType = result.recordset[0].EmployeeType;
+    const imageURL = result.recordset[0].imageURL;
 
     console.log(userID);
 
-      if (match) {
-const accessToken  = jwt.sign({"userid": userID}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: "1h"} );
-const refreshToken = jwt.sign({"userid": userID}, process.env.REFRESH_TOKEN_SECRET,{ expiresIn: "24h"} );
+    if (match) {
+      const accessToken = jwt.sign(
+        { userid: userID },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" },
+      );
+      const refreshToken = jwt.sign(
+        { userid: userID },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "24h" },
+      );
 
- let updaterefreshToken = await pool.request()
- .input('ID',sql.Int, userID)
-      .input('refreshToken',sql.VarChar,refreshToken)
-     .execute('UpdateToken');
+      let updaterefreshToken = await pool
+        .request()
+        .input("ID", sql.Int, userID)
+        .input("refreshToken", sql.VarChar, refreshToken)
+        .execute("UpdateToken");
 
-          res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
-          res.json({ accessToken, FirstName,LastName,EmployeeType});
-      } else {
-          res.status(401).json({ success: false, message: 'Invalid username or password' });
-      }
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.json({
+        accessToken,
+        userID,
+        FirstName,
+        LastName,
+        EmployeeType,
+        imageURL,
+      });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "Invalid username or password" });
+    }
   } catch (err) {
-      console.log(err);
-      res.status(500).json({ success: false, message: 'Internal server error' });
+    console.log(err);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
