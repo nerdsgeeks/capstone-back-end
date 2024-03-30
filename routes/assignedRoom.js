6d3d00dce1e7ad4b7df4df53cbdc575beb97a50f
thalhauzair1 <thalhauzair4@gmail.com>
2024-03-30 02:34:24 -0700

    SCRUM- 273 added update multiple changes to assignedRooms

--- routes/assignedRoom.js
+++ routes/assignedRoom.js
@@ -35,6 +35,19 @@ router.route("/updateAssignedRoom").put((request, response) => {
   });
 });
 
+
+router.route("/updateAssignedRoomIsCompleted").put(async (request, response) => {
+  let updateAssignedRoom = []
+  const requestRoom = request.body;
+  for (request of requestRoom) {
+   const result = await dboperations.updateAssignedRoom(request);
+   updateAssignedRoom.push(result);
+  }
+   response.status(201).json(updateAssignedRoom);
+ }
+ );
+ 
+
 router.route("/deleteAssignedRoom").post((request, response) => {
   let assignedRoom = { ...request.body };
   dboperations.deleteAssignedRoom(assignedRoom).then((result) => {
